01466a12d5405e25b2d584f7b0024347df652033
aziz-hayan <aziz@hayansystems.com>
2021-07-10 15:41:36 +0500

    v1.2

--- googleapi/models.py
+++ googleapi/models.py
@@ -5,8 +5,8 @@ from django.db.models import JSONField
 
 
 class teachers(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+    email = models.CharField(max_length=50, unique=True)
     calculations = models.BooleanField()
     clinical_pharmacy = models.BooleanField()
     pharmacy_law = models.BooleanField()
@@ -14,3 +14,6 @@ class teachers(models.Model):
     general_pharmacology = models.BooleanField()
     imagelink = models.CharField(max_length=250)
     credentials = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "teachers"
